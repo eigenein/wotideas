5158b97d174b684e2cb11d4694ea178b1acd0893
Pavel Perestoronin <eigenein@gmail.com>
2014-08-10 01:52:30 +0300

    Emails

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+from email.utils import formataddr
+
+
 ADMIN_ID = set([5589968])
 APPLICATION_ID = "b97af3d8dd57785958ae5b8a62ba61d7"
 COOKIE_SECRET = "you shall not pass"
+EMAIL_CALLBACK_TIME = 10000
+FROM = formataddr(("WoT Ideas", "no-reply@wotideas.ru"))
+NO_REPLY_PASS = "qwe123!@#"
+REPLY_TO = formataddr(("WoT Ideas", "inbox@wotideas.ru"))
